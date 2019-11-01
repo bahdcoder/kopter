68f8e92f5b062daac879656edd81ab5e12e6ee39
Kati Frantz <bahdcoder@gmail.com>
2019-11-01 22:11:16 +0100

    :beetle: Add body parser and pino middleware

--- src/Kopter.ts
+++ src/Kopter.ts
@@ -1,4 +1,11 @@
 import * as Express from 'express'
+import BodyParser from 'body-parser'
+import PinoLogger from 'express-pino-logger'
+
+export interface KopterConfig {
+    bodyParser?: BodyParser.OptionsJson | undefined | Boolean
+    pino?: PinoLogger.Options | undefined | Boolean
+}
 
 export class Kopter {
     /**
@@ -7,8 +14,39 @@ export class Kopter {
      */
     app: Express.Application
 
-    constructor(app: Express.Application) {
+    /**
+     * The default kopter configuration
+     */
+    config: KopterConfig = {
+        bodyParser: {},
+        pino: {}
+    }
+
+    constructor(app: Express.Application, config?: KopterConfig) {
         this.app = app
+
+        /**
+         * Merge the config with the default one.
+         */
+        this.config = Object.assign(this.config, config || {})
+    }
+
+    /**
+     * Registers the body-parser middleware
+     */
+    public registerBodyParser(): void {
+        this.app.use(
+            BodyParser.json(
+                (this.config.bodyParser as BodyParser.OptionsJson) || {}
+            )
+        )
+    }
+
+    /**
+     * Registers the pino logger middleware
+     */
+    public registerPinoLogger(): void {
+        this.app.use(PinoLogger((this.config.pino as PinoLogger.Options) || {}))
     }
 
     /**
@@ -18,6 +56,16 @@ export class Kopter {
      * @return Express.Application
      */
     public init(): Express.Application {
+        /**
+         * Configure body parser
+         */
+        if (this.config.bodyParser) this.registerBodyParser()
+
+        /**
+         * Configure pino logger
+         */
+        if (this.config.pino) this.registerPinoLogger()
+
         return this.app
     }
 }
--- src/__tests__/kopter.spec.ts
+++ src/__tests__/kopter.spec.ts
@@ -12,3 +12,47 @@ test('It returns the app when init function is called', () => {
 
     expect(kopter.init()).toBe(kopter.app)
 })
+
+test('It registers body parser if body parser is not set to false', () => {
+    const kopter = new Kopter(Express())
+
+    jest.spyOn(kopter, 'registerBodyParser')
+
+    kopter.init()
+
+    expect(kopter.registerBodyParser).toHaveBeenCalled()
+})
+
+test('It does not register body parser if body parser is set to false', () => {
+    const kopter = new Kopter(Express(), {
+        bodyParser: false
+    })
+
+    jest.spyOn(kopter, 'registerBodyParser')
+
+    kopter.init()
+
+    expect(kopter.registerBodyParser).toHaveBeenCalledTimes(0)
+})
+
+test('It registers pino if pino is not set to false', () => {
+    const kopter = new Kopter(Express())
+
+    jest.spyOn(kopter, 'registerBodyParser')
+
+    kopter.init()
+
+    expect(kopter.registerBodyParser).toHaveBeenCalled()
+})
+
+test('It does not register pino logger if pino is set to false', () => {
+    const kopter = new Kopter(Express(), {
+        pino: {}
+    })
+
+    jest.spyOn(kopter, 'registerPinoLogger')
+
+    kopter.init()
+
+    expect(kopter.registerPinoLogger).toHaveBeenCalled()
+})
