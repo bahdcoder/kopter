7e32fe77f9663a2fd688cebc3ded566378670808
Kati Frantz <bahdcoder@gmail.com>
2019-11-05 03:16:28 +0100

    :fish_cake: Add example queue worker

--- src/workers/queues.ts
+++ src/workers/queues.ts
@@ -3,7 +3,10 @@ import { Kopter } from '../Kopter'
 import { MAIL_SERVICE } from '../utils/constants'
 import { MailService } from '../services/mail.service'
 
-const kopter = new Kopter(undefined, {
+/**
+ * This is a temporal way of setting up queue workers with the whole application context
+ */
+new Kopter(undefined, {
     mail: {
         connection: 'ethereal',
         viewEngine: 'handlebars',
@@ -14,43 +17,28 @@ const kopter = new Kopter(undefined, {
     }
 }).initConsole()
 
-kopter.config.queue.workers.forEach((worker: any) => {
-    const Queue: any = Container.get(worker.name)
+const Queue: any = Container.get('mails.queue')
 
-    Queue.process((job: any, done: Function) => {
-        try {
-            const {
-                mailName,
-                subject,
-                customMailConfig,
-                recipients,
-                data
-            } = job.data
-            console.log(
-                '----------------------------->',
-                (Container.get(MAIL_SERVICE) as MailService).build(
-                    mailName,
-                    customMailConfig
-                )
-            )
-            ;(Container.get(MAIL_SERVICE) as MailService)
-                .build(mailName, customMailConfig)
-                .to(recipients)
-                .data({ user: data })
-                .subject(subject)
-                .send()
-                .then((result: any) => {
-                    console.log('xxxxxxxxxxxxxx', result)
-
-                    done()
-                })
-                .catch(() => {
-                    console.log('>>>>>>>>>>>>>>>>>> JOB FAILED')
-                })
-        } catch (e) {
-            console.log('xx-----xxx', e)
-        }
-    })
+Queue.process((job: any, done: Function) => {
+    try {
+        const {
+            mailName,
+            subject,
+            customMailConfig,
+            recipients,
+            data
+        } = job.data
+        ;(Container.get(MAIL_SERVICE) as MailService)
+            .build(mailName, customMailConfig)
+            .to(recipients)
+            .data({ user: data })
+            .subject(subject)
+            .send()
+            .then(done)
+            .catch(console.log)
+    } catch (e) {
+        console.error(e)
+    }
 })
 
-console.log('###### Queue worker started ! ######')
+console.log('###### Mails Queue worker started ! ######')
